18e8b3b4e431923f27a7a4239824bc4a62334116
piupiupipiii <silvynurazkia690@gmail.com>
2024-12-07 06:16:59 +0700

    add navbar in statistic page

--- resources/views/statistic.blade.php
+++ resources/views/statistic.blade.php
@@ -8,35 +8,43 @@
   <link rel="stylesheet" href="{{ asset('css/statistic.css') }}">
 </head>
 <body>
-    
-  <div class="main-container">
-    <nav class="navbar">
-      <ul>
-        <li><a href="{{ url('/home') }}">Home</a></li>
-      </ul>
-    </nav>
-    <h1 class="judul-title">Statistik Masukkan Sampah</h1>
-    <div class="statistics-wrapper">
-      <div class="picture-container">
-        <img src="image/leaf.png" alt="Statistik Gambar 1" class="statistics-chart">
-        <img src="image/trash.png" alt="Statistik Gambar 2" class="statistics-chart">
-        <img src="image/stats.png" alt="Statistik Gambar 3" class="statistics-chart">
-        <img src="image/leaf.png" alt="Statistik Gambar 4" class="statistics-chart">
-        <img src="image/leaf.png" alt="Statistik Gambar 5" class="statistics-chart">
+  <div class="container">
+    <header class="main-header">
+        <h1>Smart Trash Bin</h1>
+        <nav>
+            <ul>
+            <li><a href="{{ url('/home') }}">Home</a></li>
+            <li><a href="{{ url('/statistic') }}">Statistics</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+    <main>
+      <div class="statistics-wrapper">
+        <div class="picture-container">
+          <img src="{{ asset('image/leaf.png') }}" alt="Statistik Gambar 1" class="statistics-chart">
+          <img src="{{ asset('image/trash.png') }}" alt="Statistik Gambar 2" class="statistics-chart">
+          <img src="{{ asset('image/stats.png') }}" alt="Statistik Gambar 3" class="statistics-chart">
+          <img src="{{ asset('image/leaf.png') }}" alt="Statistik Gambar 4" class="statistics-chart">
+          <img src="{{ asset('image/leaf.png') }}" alt="Statistik Gambar 5" class="statistics-chart">
+        </div>
+        <div class="statistics-container">
+          <div class="date-column">
+            <div class="column-header">Tanggal</div>
+          </div>
+          <div class="quantity-column">
+            <div class="column-header">Jumlah Sampah Dimasukkan</div>
+          </div>
+          <div class="category-column">
+            <div class="column-header">Kategori</div>
+          </div>
+        </div>
       </div>
-      <div class="statistics-container">
-        <div class="date-column">
-          <div class="column-header">Tanggal</div>
-        </div>
-        <div class="quantity-column">
-          <div class="column-header">Jumlah Sampah Dimasukkan</div>
-        </div>
-        <div class="category-column">
-          <div class="column-header">Kategori</div>
-        </div>
-      </div>
-    </div>
+    </main>
   </div>
+
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
     function fetchData() {
@@ -45,11 +53,10 @@
         method: 'GET',
         dataType: 'json',
         success: function(response) {
-          var totalMasukkan = response.data.length;
-          $('#total-sampah-bulanan').text(totalMasukkan + ' kali');
           var dateColumn = '';
           var quantityColumn = '';
           var categoryColumn = '';
+
           response.data.forEach(function(item) {
             var timestamp = item.timestamp;
             var category = item.category;
@@ -57,6 +64,7 @@
             quantityColumn += `<div class="column-item">1 Kali</div>`;
             categoryColumn += `<div class="column-item">${category}</div>`;
           });
+
           $('.date-column').html(`<div class="column-header">Tanggal</div>${dateColumn}`);
           $('.quantity-column').html(`<div class="column-header">Jumlah Sampah Dimasukkan</div>${quantityColumn}`);
           $('.category-column').html(`<div class="column-header">Kategori</div>${categoryColumn}`);
@@ -66,6 +74,7 @@
         }
       });
     }
+
     setInterval(fetchData, 5000);
     fetchData();
   </script>
